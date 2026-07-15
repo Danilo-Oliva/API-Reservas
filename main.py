203cb81c7976f6dbe563fcad7ffbd7e1d774536f
Icaro <danilo.oliva153@gmail.com>
2026-07-14 21:45:41 -0300

    ✨ [FEAT] refactor main.py to initialize FastAPI and include user router

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from fastapi import FastAPI
 from src.database import engine, Base
+
 from src.models.usuario import Usuario
 from src.models.reserva import Reserva
 
-def inicializar_base_de_datos():
-  # crear fisicamente el archivo reservas.db y sus tablas
-  Base.metadata.create_all(bind=engine)
-  print("Base de datos y tabla creadas exitosamente")
-  
-if __name__ == "__main__":
-  inicializar_base_de_datos()
+from src.routers.usuarios import router as usuarios_routers
+
+app = FastAPI(title="Sistema de Reservas API", version="1.0.0")
+
+Base.metadata.create_all(bind=engine)
+app.include_router(usuarios_routers)
+
+
+@app.get("/")
+def leer_raiz():
+    return {"mensaje": "API de reservas funcionando correctamente"}
