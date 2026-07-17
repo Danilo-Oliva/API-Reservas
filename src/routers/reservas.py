9cdc3553a45d7e5c71aec2db56701f3e8e29cd18
Icaro <danilo.oliva153@gmail.com>
2026-07-17 12:34:22 -0300

    ✨ [FEAT] add endpoints to list all reservations and reservations by user ID with user existence validation

--- src/routers/reservas.py
+++ src/routers/reservas.py
@@ -79,3 +79,27 @@ def crear_reserva(reserva: ReservaCreate, db:Session = Depends(get_db)):
   db.refresh(nueva_reserva)
   
   return nueva_reserva
+
+# Endpoint para obtener todas las reservas registradas
+@router.get("/", response_model=list[ReservaResponse])
+def listar_todas_las_reservas(db: Session = Depends(get_db)):
+  reservas = db.query(Reserva).all()
+  return reservas
+
+# endpoint para obtener todas las reservas de un mismo usuario en especifico
+@router.get("/usuario/{usuario_id}", response_model=list[ReservaResponse])
+def listar_reservas_de_usuario(usuario_id: int, db: Session = Depends(get_db)):
+  # verificar que el usuario exista
+  usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
+  
+  # si no existe
+  if not usuario:
+    raise HTTPException(
+      status_code=404,
+      detail=f"El usuario con ID {usuario_id} no existe."
+    )
+  
+  # si existe
+  reservas_usuario = db.query(Reserva).filter(Reserva.usuario_id == usuario_id).all()
+  
+  return reservas_usuario
